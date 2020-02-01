1efa328c5e75ea38c85333fd7df2a926cfd08845
Ryan Seit <ryan@ryanseit.com>
2020-02-01 21:16:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,6 @@
 def position_taken(board, index)
   if board[index] == " " || board[index] == ""
     puts false
-  elsif board[index]
+  elsif board[index] != " " || board[index] == "" || board[index] 
   
 end
